44b5bc2841f72a46c4e31e9418a0b90f67b22671
andela-oakinrele <oluwasinmisola.akinrele@andela.com>
2017-01-23 13:25:50 +0100

    Indentation

--- src/app.js
+++ src/app.js
@@ -1,99 +1,101 @@
-(function(){
-const app = angular.module('inverted-index',['angularjs-dropdown-multiselect'])
-.run(function($rootScope){
-  $rootScope.Utils = {
-    keys: Object.keys
-  };
-});
-
-app.directive('uploadfile', () => {
-  return {
-    restrict: 'A',
-    require: '?ngModel',
-    link: (scope, elem, attrs, ngModel) => {
-      if (!ngModel){ return; }
-      elem.bind('change', (event)=>{
-        const isValid = event.target.files[0].name.indexOf(".json") > 0;
-        if(!isValid){
-          alert("Please upload a valid json file");
-          return;
-        }
-        const reader = new FileReader();
-        reader.onload = (loadEvent) => {
-          scope.$apply(()=>{
-            const file = loadEvent.target.result;
-            ngModel.$setViewValue({
-              fileName: event.target.files[0].name,
-              document: file
-            });
-          });
-        };
-        reader.readAsText(event.target.files[0]);
-      });
-    }
-  };
-});
-
-app.controller('UploadFileController', ['$timeout', '$scope', function($timeout, $scope) {
-  this.fileObject = null;
-  this.files = [];
-  this.fileNames = [];
-  this.indices = {};
-  this.myIndex = new InvertedIndex();
-  this.selectedFiles = [];
-  this.searchTerms = "";
-  this.found ={};
-
-  this.createIndex = function(fileIndex){
-    const fileName = this.fileNames[fileIndex];
-    const file = this.files[fileIndex];
-    const result = this.myIndex.createIndex(fileName,file);
-    if(typeof result === "object"){
-      this.indices[fileName] = result;
-    }else{
-      alert(result);
-    }
-  };
-
-  this.search = function(){
-    const indices = this.selectedFiles.length ? this.selectedFiles : Object.keys(this.indices);
-    this.searchTerms = this.searchTerms
-    .replace(/[^A-Za-z0-9\s]/g,'')
-    .toLowerCase()
-    .split(" ");
-    const result = this.myIndex.searchIndex(indices,this.searchTerms);
-    this.found = result;
-    this.searchTerms = "";
-  };
-
-  $scope.$watch('files.fileObject', (newVal, oldVal) => {
-    if(newVal){
-      try{
-        const parsedFile = JSON.parse(newVal.document);
-        parsedFile.map(file=>{
-          if(!file.title || !file.text){
-            alert('Please upload a properly formatted file');
-            return;
-          }
+(function() {
+    const app = angular.module('inverted-index', [])
+        .run(function($rootScope) {
+            $rootScope.Utils = {
+                keys: Object.keys
+            };
         });
-        this.files.push(parsedFile);
-      this.fileNames.push(newVal.fileName);
-      }catch(err){
-        console.log(err);
-        alert("Please upload a valid json file");
-          return;
-      }
-      this.fileObject = null;
-    }
-  });
 
-    $scope.getFileNamesSizeAsArray = function($index){
-      const arr = [];
-      for(i = 0; i < $scope.files[$index].length; i++){
-        arr.push(i);
-      }
-      return arr;
-    };
-}]);
+    app.directive('uploadfile', () => {
+        return {
+            restrict: 'A',
+            require: '?ngModel',
+            link: (scope, elem, attrs, ngModel) => {
+                if (!ngModel) { return; }
+                elem.bind('change', (event) => {
+                    const isValid = event.target.files[0].name.indexOf(".json") > 0 ;
+                    if (!isValid) {
+                       Materialize.toast('Please upload a valid json file', 4000);
+                        return;
+                    }
+
+                    const reader = new FileReader();
+                    reader.onload = (loadEvent) => {
+                        scope.$apply(() => {
+                            const file = loadEvent.target.result;
+                            ngModel.$setViewValue({
+                                fileName: event.target.files[0].name,
+                                document: file
+                            });
+                        });
+                    };
+                    reader.readAsText(event.target.files[0]);
+                });
+            }
+        };
+    });
+
+    app.controller('UploadFileController', ['$timeout', '$scope', function($timeout, $scope) {
+        this.fileObject = null;
+        this.files = [];
+        this.fileNames = [];
+        this.indices = {};
+        this.myIndex = new InvertedIndex();
+        this.selectedFiles = [];
+        this.searchTerms = "";
+        this.found = {};
+
+        this.createIndex = function(fileIndex) {
+            const fileName = this.fileNames[fileIndex];
+            const file = this.files[fileIndex];
+            const result = this.myIndex.createIndex(fileName, file);
+            if (typeof result === "object") {
+                this.indices[fileName] = result;
+            } else {
+                alert(result);
+            }
+        };
+
+        this.search = function() {
+            const indices = this.selectedFiles.length ? this.selectedFiles : Object.keys(this.indices);
+            this.searchTerms = this.searchTerms
+                .replace(/[^A-Za-z0-9\s]/g, '')
+                .toLowerCase()
+                .split(" ");
+            const result = this.myIndex.searchIndex(indices, this.searchTerms);
+            this.found = result;
+            this.searchTerms = "";
+        };
+
+        $scope.$watch('files.fileObject', (newVal, oldVal) => {
+            if (newVal) {
+                try {
+                    const parsedFile = JSON.parse(newVal.document);
+                    parsedFile.map(file => {
+                        if (!file.title || !file.text) {
+                            throw new Error('format');
+                        }
+                    });
+                    this.files.push(parsedFile);
+                    this.fileNames.push(newVal.fileName);
+                } catch (err) {
+                    if(err.message === 'format'){
+                        Materialize.toast('Please upload a properly formatted file', 4000) ;
+                    }else{
+                        Materialize.toast('Please upload a valid json file', 4000) ;
+                    }
+                }
+                this.fileObject = null;
+            }
+        });
+
+        $scope.getFileNamesSizeAsArray = function($index) {
+            const arr = [];
+            for (i = 0; i < $scope.files[$index].length; i++) {
+                arr.push(i);
+            }
+            return arr;
+        };
+    }]);
 
 })();
--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,90 +1,89 @@
 class InvertedIndex {
-  constructor() {
-   this.indices = [];
-   this.errorMessage= "";
-  }
-
-  getIndex(filename){
-    return this.indices[filename];
-  }
-
-  validateDoc(parseDoc){
-    let isJSONObject = (typeof parseDoc === "object");
-    let isNotEmpty = parseDoc.length > 0;
-    if(!isNotEmpty){
-     this.errorMessage = 'the file is empty';
+    constructor() {
+        this.indices = [];
+        this.errorMessage = "";
     }
 
-    let isValidStructure = true;
-    parseDoc.map(file => {
-      if (!file.title ||
-          typeof file.title !== 'string' ||
-          !file.text ||
-          typeof file.text !== 'string'){
-        isValidStructure = false;
-      }
-    });
-    if(!isValidStructure){
-      this.errorMessage = 'the file structure is invalid';
+    getIndex(filename) {
+        return this.indices[filename];
     }
 
-    return isJSONObject && isNotEmpty && isValidStructure;
-  }
+    validateDoc(parseDoc) {
+        let isJSONObject = (typeof parseDoc === "object");
+        let isNotEmpty = parseDoc.length > 0;
+        if (!isNotEmpty) {
+            this.errorMessage = 'the file is empty';
+        }
 
-  createIndex(filename, document){
-    let index = {};
-    if(this.validateDoc(document)){
-      document.map((sentence, count)=>{
-       `${sentence.title} ${sentence.text}`
-       .replace(/[^A-Za-z0-9\s]/g,'')
-       .toLowerCase()
-       .split(" ")
-       .map(word=>{
-         if(index[word] && index[word].indexOf(count) === -1){
-           index[word].push(count);
-         }else{
-           index[word] = [count];
-         }
-       });
-     });
-      this.indices[filename] = index;
-     return index;
-      }
-    return this.errorMessage;
-  }
+        let isValidStructure = true;
+        parseDoc.map(file => {
+            if (!file.title ||
+                typeof file.title !== 'string' ||
+                !file.text ||
+                typeof file.text !== 'string') {
+                isValidStructure = false;
+            }
+        });
+        if (!isValidStructure) {
+            this.errorMessage = 'the file structure is invalid';
+        }
 
-  searchIndex(indicesParam,...args){
-  indicesParam = indicesParam ? indicesParam : Object.keys(this.indices);
-  const result = {};
-  const searchTerms = args.flatten();
-  //console.log(indicesParam, searchTerms);
-  for(let searchTerm of searchTerms){
-    result[searchTerm] = {};
-    for(let index of indicesParam){
-      // console.log(index,searchTerm);
-      result[searchTerm][index] = this.search(index,searchTerm);
+        return isJSONObject && isNotEmpty && isValidStructure;
     }
-  }
-  return result;
-}
 
-search(index,term){
-  return this.indices[index][term] ? this.indices[index][term] : [];
-}
+    createIndex(filename, document) {
+        let index = {};
+        if (this.validateDoc(document)) {
+            document.map((sentence, count) => {
+                `${sentence.title} ${sentence.text}`
+                .replace(/[^A-Za-z0-9\s]/g, '')
+                    .toLowerCase()
+                    .split(" ")
+                    .map(word => {
+                        if (index[word] && index[word].indexOf(count) === -1) {
+                            index[word].push(count);
+                        } else {
+                            index[word] = [count];
+                        }
+                    });
+            });
+            this.indices[filename] = index;
+            return index;
+        }
+        return this.errorMessage;
+    }
+
+    searchIndex(indicesParam, ...args) {
+        indicesParam = indicesParam ? indicesParam : Object.keys(this.indices);
+        const result = {};
+        const searchTerms = args.flatten();
+        //console.log(indicesParam, searchTerms);
+        for (let searchTerm of searchTerms) {
+            result[searchTerm] = {};
+            for (let index of indicesParam) {
+                // console.log(index,searchTerm);
+                result[searchTerm][index] = this.search(index, searchTerm);
+            }
+        }
+        return result;
+    }
+
+    search(index, term) {
+        return this.indices[index][term] ? this.indices[index][term] : [];
+    }
 }
 
 // extends the array object
-Array.prototype.flatten = function flatten(){
-   let flat = [];
-   for (let i = 0, l = this.length; i < l; i++){
-       const type = Object.prototype.toString.call(this[i]).split(' ').pop().split(']').shift().toLowerCase();
-       if (type) { flat = flat.concat(/^(array|collection|arguments|object)$/.test(type) ? flatten.call(this[i]) : this[i]); }
-   }
-   return flat;
+Array.prototype.flatten = function flatten() {
+    let flat = [];
+    for (let i = 0, l = this.length; i < l; i++) {
+        const type = Object.prototype.toString.call(this[i]).split(' ').pop().split(']').shift().toLowerCase();
+        if (type) { flat = flat.concat(/^(array|collection|arguments|object)$/.test(type) ? flatten.call(this[i]) : this[i]); }
+    }
+    return flat;
 };
 
-//
-if(typeof module === "object" && module.exports) {
-  module.exports = InvertedIndex;
-}
 
+if (typeof module === "object" && module.exports) {
+    module.exports = InvertedIndex;
+}
