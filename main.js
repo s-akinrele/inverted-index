1fb26287168595d4f7e223df1d6a0a869feb6366
andela-oakinrele <oluwasinmisola.akinrele@andela.com>
2017-01-23 19:53:51 +0100

    move port to environment variables

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ const path = require('path');
 
 app.use(express.static((__dirname)));
 
-let server = app.listen(3000, function(){
-  console.log('Server listening on port 3000');
+let server = app.listen(process.env.PORT || 5000, function() {
+    console.log('Server listening on port 3000');
 });
